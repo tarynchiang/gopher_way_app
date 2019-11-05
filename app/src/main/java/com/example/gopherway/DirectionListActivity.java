e839a9e5ddb3b3eb497ad1873041ac829c003a2d
mason <kozak042@umn.edu>
2019-11-05 11:35:19 -0600

    Added framework for list of directions. Some example directions with mockup icons.

---
+++ app/src/main/java/com/example/gopherway/DirectionListActivity.java
@@ -0,0 +1,56 @@
+package com.example.gopherway;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirectionListActivity extends AppCompatActivity {
+    List<Direction> directionList;
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction_list);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        directionList = new ArrayList<>();
+
+
+        //Example directions, will be generated after path is calculated
+        directionList.add(
+                new Direction(
+                        1,
+                        "Take right",
+                        "At end of hall",
+                        20,
+                        R.drawable.right));
+
+        directionList.add(
+                new Direction(
+                        1,
+                        "Walk straight",
+                        "Until doorway",
+                        60,
+                        R.drawable.forward));
+
+        directionList.add(
+                new Direction(
+                        1,
+                        "Go Up stairs on left",
+                        "",
+                        0,
+                        R.drawable.upstair));
+
+        DirectionAdapter adapter = new DirectionAdapter(this, directionList);
+
+        recyclerView.setAdapter(adapter);
+    }
+}
