8776d4a33cbf5b46378ac30ef3a7a41009e46d31
Alec Johnson <joh13085@umn.edu>
2019-11-06 15:41:39 -0600

    Begining framework for navigation page and directional information text

--- app/src/main/java/com/example/gopherway/DisplayNavigationOptions.java
+++ app/src/main/java/com/example/gopherway/DisplayNavigationOptions.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayNavigationOptions extends AppCompatActivity {
+
+    Button backButton;
+    Button infoButton;
+    Button nextButton;
+    TextView directions;
+    int routeOption=0;
+    String[][] directionArr = UniversityLocations.getDirectionArr();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +25,24 @@ public class DisplayNavigationOptions extends AppCompatActivity {
         int currentLocation = (int)(intent.getIntExtra("CurrentLocation",0));
         int destination = (int)(intent.getIntExtra("Destination",0));
 
-        TextView textView2 = (TextView)findViewById(R.id.textView2);
-        textView2.setText(Integer.toString(currentLocation));
-        TextView textView3 = (TextView)findViewById(R.id.textView3);
-        textView3.setText(Integer.toString(destination));
+        //Decide which route option to begin displaying
+        if(currentLocation == 1 && destination ==2){routeOption=1;}
+        else if(currentLocation == 3 && destination ==4){routeOption=2;}
+        else if(currentLocation == 5 && destination ==6){routeOption=3;}
+
+        //TextView for Direction Information -- Display first Step
+        directions = (TextView)findViewById(R.id.textView2);
+        directions.setText(directionArr[routeOption][0]);
+
+        //Bottom Page Button Navigation
+        backButton = (Button) findViewById(R.id.button2);
+        infoButton = (Button) findViewById(R.id.button3);
+        nextButton = (Button) findViewById(R.id.button4);
     }
+
+    public void backButtonClick(){}
+
+    public void moreInfoClick(){}
+
+    public void nextButtonClick(){}
 }
--- app/src/main/java/com/example/gopherway/UniversityLocations.java
+++ app/src/main/java/com/example/gopherway/UniversityLocations.java
@@ -6,11 +6,12 @@ import java.util.Set;
 
 public class UniversityLocations {
 
-    UniversityLocations(){}
+    UniversityLocations() {
+    }
 
     public static Map<String, Integer> buildingList = createMap();
 
-    private static Map<String, Integer> createMap(){
+    private static Map<String, Integer> createMap() {
         Map<String, Integer> map = new HashMap<String, Integer>();
         map.put("McNamara", 1);
         map.put("Moos Tower", 2);
@@ -19,7 +20,16 @@ public class UniversityLocations {
         return map;
     }
 
-    public static int findBuilding(String building){
+    public static int findBuilding(String building) {
         return buildingList.get(building);
+    }
+
+    public static String[][] getDirectionArr() {
+        String[] error = {"Error: Path not found"};
+        String[] McNamaraToMoosTower = {};//TODO: Fill in path directions
+        String[] RapsonToFord = {};//TODO: Fill in path directions
+        String[] CoffmanToDiehl = {};//TODO: Fill in path directions
+        String[][] result = {error, McNamaraToMoosTower, RapsonToFord, CoffmanToDiehl};
+        return result;
     }
 }
