ee6d5c044a4d6d2107e2cd899d81c4d7ba29f205
Your Name <kozak042@umn.edu>
2019-11-11 15:30:19 -0600

    Macnamara directions added to UniversityLocations

--- app/src/main/java/com/example/gopherway/UniversityLocations.java
+++ app/src/main/java/com/example/gopherway/UniversityLocations.java
@@ -26,7 +26,21 @@ public class UniversityLocations {
 
     public static String[][] getDirectionArr() {
         String[] error = {"Error: Path not found"};
-        String[] McNamaraToMoosTower = {};//TODO: Fill in path directions
+        //14 steps
+        String[] McNamaraToMoosTower = {"Start at Fireplace in Macnamara.",
+                "Take the stairs to the right of the fireplace down.",
+                "Follow the tunnel until the elevator.",
+                "Take the elevator up to the skyway (level 2).",
+                "Take a right and enter graduate hotel.",
+                "Take a right.",
+                "Follow right wall until brown double doors.",
+                "Follow skyway to the parking garage, go through the first set of doors.",
+                "Go through the doors to the left.",
+                "Follow the hallway past the first door.",
+                "Go through the tan double doors",
+                "Follow hallway to the end and go through the doors.",
+                "Take elevator down to the basement (level B).",
+                "Take a right and follow hallway to Moos Tower."};//TODO: Fill in path directions
         String[] RapsonToFord = {};//TODO: Fill in path directions
         String[] CoffmanToDiehl = {};//TODO: Fill in path directions
         String[][] result = {error, McNamaraToMoosTower, RapsonToFord, CoffmanToDiehl};
