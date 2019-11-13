370d07d1c7afd04079684c6b3ba6f4725410663b
mason <kozak042@umn.edu>
2019-11-13 16:29:24 -0600

    Changed location input to dropdown menus

--- app/src/main/java/com/example/gopherway/MainActivity.java
+++ app/src/main/java/com/example/gopherway/MainActivity.java
@@ -15,7 +15,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+/*
+        Spinner select_start = (Spinner) findViewById(R.id.spinner_start);
+        Spinner select_destination = (Spinner) findViewById(R.id.spinner_destination);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.buildings));
+
+        //myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        select_start.setAdapter(myAdapter);
+        select_destination.setAdapter(myAdapter);
+*/
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_saved_locations, R.id.nav_travel_history,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
@@ -64,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**Called when the user taps the Search button */
     public void search(View view){
+        /*
         EditText editText = (EditText) findViewById(R.id.editText);
         EditText editText2 = (EditText) findViewById(R.id.editText2);
         String currentLocation = editText.getText().toString();
@@ -72,11 +85,19 @@ public class MainActivity extends AppCompatActivity {
         int x = UniversityLocations.findBuilding(currentLocation);
         int y = UniversityLocations.findBuilding(destination);
         //Intent intent = new Intent(this, DisplayNavigationOptions.class);
+        */
 
+        //Changed Input to selection from list rather than reading in a string
+        Spinner select_start = (Spinner) findViewById(R.id.spinner_start);
+        Spinner select_destination = (Spinner) findViewById(R.id.spinner_destination);
+        //String currentLocation = select_start.getText().toString();
+        //String destination = select_destination.getText().toString();
+        int startID = select_start.getSelectedItemPosition();
+        int destinationID = select_destination.getSelectedItemPosition();
         //short circuiting to this screen for now
         Intent intent = new Intent(this, DisplayNavigationOptions.class);
-        intent.putExtra("CurrentLocation", x);
-        intent.putExtra("Destination", y);
+        intent.putExtra("CurrentLocation", startID);
+        intent.putExtra("Destination", destinationID);
         startActivity(intent);
     }
 }
