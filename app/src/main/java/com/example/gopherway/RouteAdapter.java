36a8a01ffeabcc464e8c07b08f3a6d0470d32397
yi-ting Chiang <tarynchiang@gmail.com>
2019-11-17 00:26:38 -0600

    add route related file

---
+++ app/src/main/java/com/example/gopherway/RouteAdapter.java
@@ -0,0 +1,69 @@
+package com.example.gopherway;
+
+import android.content.Context;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.List;
+
+public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.RouteViewHolder> {
+
+
+    //this context we will use to inflate the layout
+    private Context mCtx;
+
+    //we are storing all the products in a list
+    private List<Route> routeList;
+
+    //getting the context and product list with constructor
+    public RouteAdapter(Context mCtx, List<Route> routeList) {
+        this.mCtx = mCtx;
+        this.routeList = routeList;
+    }
+
+    @Override
+    public RouteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.layout_directions, null);
+        return new RouteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RouteViewHolder holder, int position) {
+        //getting the product of the specified position
+        Route route = routeList.get(position);
+
+        //binding the data with the viewholder views
+        holder.textViewStart.setText(route.getStart());
+        holder.textViewEnd.setText(route.getEnd());
+
+//        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return routeList.size();
+    }
+
+
+    class RouteViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewStart, textViewEnd;
+        ImageView imageView;
+
+        public RouteViewHolder(View itemView) {
+            super(itemView);
+
+            textViewStart = itemView.findViewById(R.id.textViewStart);
+            textViewEnd = itemView.findViewById(R.id.textViewEnd);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gopherway/RouteListActivity.java
@@ -0,0 +1,53 @@
+package com.example.gopherway;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteListActivity extends AppCompatActivity {
+
+    //a list to store all the products
+    List<Route> routeList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_saved_locations);
+
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        routeList = new ArrayList<>();
+
+
+        //adding some items to our list
+        routeList.add(
+                new Route(
+                        1,
+                        "McNamara",
+                        "Moos"));
+
+        routeList.add(
+                new Route(
+                        1,
+                        "Rapson",
+                        "Coffman"));
+
+        //creating recyclerview adapter
+        RouteAdapter adapter = new RouteAdapter(this, routeList);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+    }
+}
